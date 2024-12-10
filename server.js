0d840eeba65fd2264be4f6af567d49f972e26d60
arun <arun@aruns-MBP.bbrouter>
2024-12-11 00:18:24 +0530

    dep

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 10000 
 });
 
 // Razorpay initialization
